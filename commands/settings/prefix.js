1e6d4c0ea3c21f7e0e5b5c1d940b05858845939d
Jeotique <71186872+Jeotique@users.noreply.github.com>
2021-11-11 20:32:22 +0100

    change get to getOne for find the prefix

--- commands/settings/prefix.js
+++ commands/settings/prefix.js
@@ -17,7 +17,7 @@ module.exports = {
             if(!message.member.permissions.has('MANAGE_GUILD') && !client.config.owners.includes(message.author.id)) return message.reply(`:x: You don't have the permission to use this command.`).catch(e=>{})
             let newprefix = args[0]
             if(!newprefix) return message.channel.send("You didn't gave me any prefix.").catch(e=>{})
-            let currentprefix = await client.db.get('settings', [{serverid: message.guildId}], 'prefix') || client.config.prefix
+            let currentprefix = await client.db.getOne('settings', [{serverid: message.guildId}], 'prefix') || client.config.prefix
             if(newprefix===currentprefix) return message.channel.send(`The prefix is exacly the same of my current one.`).catch(e=>{})
             client.db.set('settings', [{serverid: message.guildId}], 'prefix', newprefix)
             await message.channel.send(`My new prefix is \`${newprefix}\``).catch(e=>{})
